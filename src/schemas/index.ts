f6283408a0e10711c8b361247dd396770d775229
NeilRow56 <neilrowland56@gmail.com>
2024-03-05 10:54:23 +0000

    Updated form on settings page and rooy layout for color themes

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,48 +1,49 @@
-import * as z from "zod";
+import * as z from 'zod'
 
 export const LoginSchema = z.object({
   email: z.string().email({
-    message: "Valid email is required",
+    message: 'Valid email is required',
   }),
   password: z.string().min(1, {
-    message: "Password is required",
+    message: 'Password is required',
   }),
-});
+})
 
 export const RegisterSchema = z
   .object({
     email: z.string().email({
-      message: "Valid email is required",
+      message: 'Valid email is required',
     }),
     password: z
       .string()
       .min(6, {
-        message: "Minimum 6 characters required",
+        message: 'Minimum 6 characters required',
       })
-      .max(50, "Password must be less than 50 characters"),
+      .max(50, 'Password must be less than 50 characters'),
     confirmPassword: z
       .string()
       .min(6, {
-        message: "Minimum 6 characters required",
+        message: 'Minimum 6 characters required',
       })
-      .max(50, "Password must be less than 50 characters"),
+      .max(50, 'Password must be less than 50 characters'),
     firstName: z
       .string()
-      .min(2, { message: "First Name must be at least 2 characters" })
-      .max(45, { message: "First Name must be less than 45 characters" })
-      .regex(new RegExp("^[a-zA-Z]+$"), "No special character allowed!"),
+      .min(2, { message: 'First Name must be at least 2 characters' })
+      .max(45, { message: 'First Name must be less than 45 characters' })
+      .regex(new RegExp('^[a-zA-Z]+$'), 'No special character allowed!'),
     lastName: z
       .string()
-      .min(2, { message: "Last Name must be at least 2 characters" })
-      .max(45, { message: "Last Name must be less than 45 characters" })
-      .regex(new RegExp("^[a-zA-Z]+$"), "No special character allowed!"),
+      .min(2, { message: 'Last Name must be at least 2 characters' })
+      .max(45, { message: 'Last Name must be less than 45 characters' })
+      .regex(new RegExp('^[a-zA-Z]+$'), 'No special character allowed!'),
+
     accepted: z.literal(true, {
       errorMap: () => ({
-        message: "Please accept all terms",
+        message: 'Please accept all terms',
       }),
     }),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Password and confirm password doesn't match!",
-    path: ["confirmPassword"],
-  });
+    path: ['confirmPassword'],
+  })
